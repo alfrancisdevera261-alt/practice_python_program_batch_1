6584be8cd4cb61b5f18b8d212f4fedba66900087
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-09 11:02:05 +0800

    checking what is the bigger number between 1 and 2

--- print_the_bigger_number_of_2_numbers.py
+++ print_the_bigger_number_of_2_numbers.py
@@ -2,3 +2,8 @@
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter a second number: "))
 
+#check the the bigger number in number1 and number2
+if number1 > number2:
+    print(f"The number {number1} is bigger than {number2}")
+else:
+    print(f"The number {number2} is bigger than {number1}")
