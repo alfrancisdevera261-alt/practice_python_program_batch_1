c290136aff72784c51f0949a01d5a4af131a75d0
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-09 11:11:10 +0800

    checking if the numbers is the same

--- checking_if_both_number_is_the_same.py
+++ checking_if_both_number_is_the_same.py
@@ -1,3 +1,9 @@
 #input of 2 numbers
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter a second number: "))
+
+#checking if the numbers is the same
+if number1 == number2:
+    print("Both numbers are the same")
+else:
+    print("The numbers is not the same")
