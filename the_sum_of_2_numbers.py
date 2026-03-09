b0aa224da3fce90f234145f2e1c771e91208189b
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-09 11:18:16 +0800

    making a more shorter of adding the 2 numbers

--- the_sum_of_2_numbers.py
+++ the_sum_of_2_numbers.py
@@ -1,6 +1,4 @@
 #input of 2 numbers
-number1 = int(input("Enter a number: "))
-number2 = int(input("Enter a second number: "))
+number = int(input("Enter a number: "))
+number += int(input("Enter a second number: "))
 
-#adding the 2 number
-print(number1 + number2)
