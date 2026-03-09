0b9bc5fdf183395e131c701923eeb64650def18f
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-09 11:04:37 +0800

    added new comparison if both number is the same

--- print_the_bigger_number_of_2_numbers.py
+++ print_the_bigger_number_of_2_numbers.py
@@ -5,5 +5,7 @@ number2 = int(input("Enter a second number: "))
 #check the the bigger number in number1 and number2
 if number1 > number2:
     print(f"The number {number1} is bigger than {number2}")
-else:
+elif number2 > number1:
     print(f"The number {number2} is bigger than {number1}")
+else:
+    print("Both number is the same")
